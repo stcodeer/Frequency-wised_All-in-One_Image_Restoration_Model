69063532ddf68237030503dd997b7410d0513065
stcodeer <stcodeer@gmail.com>
2023-08-10 12:00:38 +0800

    change visualization utilities

--- plot_performance_curve.py
+++ plot_performance_curve.py
@@ -3,14 +3,13 @@ import os
 import re
 import numpy as np
 
+from option import options as opt
 
-path = 'output/crop_test_imgs_size_128_de_type_denoising_15_denoising_25_denoising_50_deraining_test_de_type_denoising_bsd68_15_denoising_bsd68_25_denoising_bsd68_50_deraining'
+f = np.zeros((len(opt.test_de_type), opt.epochs // 100))
 
-f = np.zeros((4, 10))
-
-for i in range(100, 1100, 100):
+for i in range(100, opt.epochs + 100, 100):
     file_name = 'epoch_%s_results.log'%str(i)
-    file = open(os.path.join(path, file_name), 'r')
+    file = open(os.path.join(opt.output_path, file_name), 'r')
     Lines = file.readlines()
     file.close()
     
@@ -20,14 +19,10 @@ for i in range(100, 1100, 100):
             if strings[pos] == 'PSNR':
                 f[idx][(i - 100) // 100] = float(strings[pos + 2])
                 
-                
-
-print(f)
-                
-plot_curve(f, x_range=(100, 1100, 100), 
+plot_curve(f, x_range=(100, opt.epochs + 100, 100), 
            labels=('denoising_15', 'denoising_25', 'denoising_50', 'deraining'), 
            xlabel='Epochs', 
            ylabel='PSNR', 
-           save_path=os.path.join(path, 'PSNR_curve.png'))            
+           save_path=os.path.join(opt.output_path, 'PSNR_curve.png'))            
     
 
